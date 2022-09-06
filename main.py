f295bf8b6e6c4b58ce106897dc907a4a87278936
SashaSemenishchev <sashasemenishchev@gmail.com>
2022-09-06 16:12:23 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+from torrent.main import TorrentClient
+from urllib import request
+from flask import Flask, render_template, redirect, send_from_directory
+from werkzeug.utils import secure_filename
+from uuid import uuid4
+import os
+from flask_socketio import SocketIO, emit
+
+UPLOAD_FOLDER = '/runs/'
+ALLOWED_EXTENSIONS = {'torrent', "bittorrent"}
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+app = Flask("TorrentDownloader")
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config["downloading"] = {}
+socket = SocketIO(app)
+
+@app.route("/")
+@app.route("/index")
+def upload():
+    return render_template("index.html")
+
+@app.route("/download-start", methods=["POST"])
+def start_download():
+    uuid = str(uuid4())
+    if "file" not in request.files:
+        return "No file"
+    file = request.files["file"]
+    if file.filename == '':
+            return redirect("/")
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        uuid = request.args.get("download_id")
+        path = os.path.join(app.config['UPLOAD_FOLDER'] + "uploads/", f"{uuid}.torrent")
+        file.save(path)
+        torrent = TorrentClient(path)
+        torrent.start()
+        app.config["downloading"][uuid] = torrent
+    return redirect(f"/downloading/?download_id={uuid}&filename={filename}")
+
+@app.route("/downloading")
+def upload_status():
+    return render_template("downloading.html", downloading=request.args.get("filename"), download_id=request.args.get("download_id"))
+
+@app.route("/download/<path:filename>")
+def download(filename):
+    uploads = app.config['UPLOAD_FOLDER'] + "downloads/"
+    return send_from_directory(directory=uploads, filename=filename + ".zip")
+
+@socket.on("connection_data")
+def on_connection(message):
+    def on_progress(progress):
+        emit("download_update", {"data": str(progress)})
+    def on_finish():
+        del app.config["downloading"][message["data"]]
+        emit("download_finish")
+    torrent = app.config["downloading"][message["data"]]
+    torrent.set_on_progress(on_progress)
+    torrent.set_on_finish(on_finish)
+
+if __name__ == "__main__":
+    app.run(port=8080, debug=True)
---
+++ torrent/main.py
@@ -0,0 +1,122 @@
+from concurrent.futures import thread
+import sys
+from block import State
+import zipfile
+import os
+
+__author__ = 'alexisgallepe'
+
+import time
+import peers_manager
+import pieces_manager
+import torrent
+import tracker
+import logging
+from threading import Thread
+import message
+
+def on_progress_dummy(p):
+    pass
+
+def on_finish_dummy():
+    pass
+
+class TorrentClient:
+    percentage_completed = -1
+    last_log_line = ""
+
+    def __init__(self, file, uuid):
+        try:
+            torrent_file = file
+        except IndexError:
+            logging.error("No torrent file provided!")
+            sys.exit(0)
+        self.torrent = torrent.Torrent().load_from_path(torrent_file)
+        self.tracker = tracker.Tracker(self.torrent)
+
+        self.pieces_manager = pieces_manager.PiecesManager(self.torrent)
+        self.peers_manager = peers_manager.PeersManager(self.torrent, self.pieces_manager)
+        self.uuid = uuid
+        self.peers_manager.start()
+        self.on_progress = on_progress_dummy
+        self.on_finish = on_finish_dummy
+        logging.info("PeersManager Started")
+        logging.info("PiecesManager Started")
+
+    def set_on_progress(self, func):
+        self.on_progress = func
+
+    def set_on_finish(self, func):
+        self.on_finish = func
+    def run(self):
+        while not self.pieces_manager.all_pieces_completed():
+            if not self.peers_manager.has_unchoked_peers():
+                time.sleep(1)
+                logging.info("No unchocked peers")
+                continue
+
+            for piece in self.pieces_manager.pieces:
+                index = piece.piece_index
+
+                if self.pieces_manager.pieces[index].is_full:
+                    continue
+
+                peer = self.peers_manager.get_random_peer_having_piece(index)
+                if not peer:
+                    continue
+
+                self.pieces_manager.pieces[index].update_block_status()
+
+                data = self.pieces_manager.pieces[index].get_empty_block()
+                if not data:
+                    continue
+
+                piece_index, block_offset, block_length = data
+                piece_data = message.Request(piece_index, block_offset, block_length).to_bytes()
+                peer.send_to_peer(piece_data)
+
+            self.display_progression()
+
+            time.sleep(0.1)
+        self.display_progression()
+        with zipfile.ZipFile(f"../runs/downloads/{self.uuid}.zip", "w") as zip:
+            for piece in self.pieces_manager.files:
+                zip.write(piece)
+        for piece in self.pieces_manager.files:
+            os.remove(piece)
+        self._exit_threads()
+    def start(self):
+        peers_dict = self.tracker.get_peers_from_trackers()
+        self.peers_manager.add_peers(peers_dict.values())
+        thread = Thread(daemon=True, target=self.run)
+        thread.start()
+        
+    def display_progression(self):
+        new_progression = 0
+
+        for i in range(self.pieces_manager.number_of_pieces):
+            for j in range(self.pieces_manager.pieces[i].number_of_blocks):
+                if self.pieces_manager.pieces[i].blocks[j].state == State.FULL:
+                    new_progression += len(self.pieces_manager.pieces[i].blocks[j].data)
+
+        if new_progression == self.percentage_completed:
+            return
+
+        # number_of_peers = self.peers_manager.unchoked_peers_count()
+        percentage_completed = float((float(new_progression) / self.torrent.total_length) * 100)
+        self.on_progress(percentage_completed)
+
+        # current_log_line = "Connected peers: {} - {}% completed | {}/{} pieces".format(number_of_peers,
+        #                                                                                  round(percentage_completed, 2),
+        #                                                                                  self.pieces_manager.complete_pieces,
+        #                                                                                  self.pieces_manager.number_of_pieces)
+        # if current_log_line != self.last_log_line:
+        #     print(current_log_line)
+
+        # self.last_log_line = current_log_line
+        self.percentage_completed = new_progression
+
+    def _exit_threads(self):
+        self.peers_manager.is_active = False
+        self.on_finish()
+
